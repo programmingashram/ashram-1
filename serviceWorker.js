32311841389182f530074e82b1bd6d2336f19cad
ayushman76 <avenuedesign768@gmail.com>
2023-01-06 09:23:18 +0530

    added changes

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,18 +1,18 @@
-const staticDevCoffee = "dev-coffee-site-v1";
+const staticDevCoffee = "programming-ashram-v1";
 const assets = [
   "/",
-  "/index.html",
+  "/ashram-1/index.html",
   "/assest/theming/style.css",
   "/js/app.js",
-  "/assest/images/icons/coffee1.jpg",
-  "/assest/images/icons/coffee2.jpg",
-  "/assest/images/icons/coffee3.jpg",
-  "/assest/images/icons/coffee4.jpg",
-  "/assest/images/icons/coffee5.jpg",
-  "/assest/images/icons/coffee6.jpg",
-  "/assest/images/icons/coffee7.jpg",
-  "/assest/images/icons/coffee8.jpg",
-  "/assest/images/icons/coffee9.jpg"
+//   "./assest/images/icons/coffee1.jpg",
+//   "./assest/images/icons/coffee2.jpg",
+//   "./assest/images/icons/coffee3.jpg",
+//   "./assest/images/icons/coffee4.jpg",
+//   "./assest/images/icons/coffee5.jpg",
+//   "./assest/images/icons/coffee6.jpg",
+//   "./assest/images/icons/coffee7.jpg",
+//   "./assest/images/icons/coffee8.jpg",
+//   "./assest/images/icons/coffee9.jpg"
 ];
 
 self.addEventListener("install", installEvent => {
