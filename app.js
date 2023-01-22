62e0a4bb43dfa4b1e27a46c8a188fb68744fd4e9
programmingashram <avenuedesign7658@gmail.com>
2023-01-22 10:28:10 +0530

    added

--- app.js
+++ app.js
@@ -1,49 +1,104 @@
 let blog = {
-   blogList1: {
-        "Title":"JavaScript latest Version in 2021.",
-        "image":"https://venturebeat.com/wp-content/uploads/2018/01/javascript.jpg?fit=1800%2C1116&strip=all",
-        "Descriptionshort":"ECMAScript is the official name of the language. ECMAScript",
-        "Descriptionfull":" versions have been abbreviated to ES1, ES2, ES3, ES5, and ES6. Since 2016 new versions are named by year (ECMAScript 2016 / 2017 / 2018).",
-        "blogid":"blog1"
-     },
-     blogList2:{
-        "Title":"Most asking question is what is current Bootsrap version ?",
-        "image":"https://labs.tadigital.com/wp-content/uploads/2020/03/MicrosoftTeams-image.png",
-        "Descriptionshort":"Bootstrap 5 stable was released on May 5, 2021.",
-        "Descriptionfull":"  Bootstrap  5 beta 3 was released on March 22, 2021. Bootstrap 5 beta 2 was released on February 10, 2021. Bootstrap 5 beta 1 was released on December 7, 2020.",
-        "blogid":"blog2"
-     },
-     blogList3: {
-        "Title":"Most asking question in interview which is current PHP version ?",
-        "image":"https://sdtimes.com/wp-content/uploads/2015/07/0715.sdt-php7.jpg",
-        "Descriptionshort":"Currently, the latest stable version is PHP 7.4. ",
-        "Descriptionfull":"Supported PHP versions and lifecycle.",
-        "blogid":"blog3"
-     },
-     blogList4:{
-        "Title":"What is Sass ?",
-        "image":"https://images.iambacon.co.uk/blog/sass.png",
-        "Descriptionshort":"Sass is a preprocessor scripting language that is interpreted ",
-        "Descriptionfull":"or compiled into Cascading Style Sheets. SassScript is the scripting language itself. Sass consists of two syntaxes. The original syntax, called 'the indented syntax,' uses a syntax similar to Haml.",
-        "blogid":"blog4"
-     },
-     blogList5:{
-       "image":"http://programmingashram.in/images/javascript-defer.png",
-       "Title":"defer in javascript.",
-       "Descriptionshort":"The defer is a Boolean value, used to indicate that script is executed after",
-       "Descriptionfull":"the document has been parsed. It works only with external scripts (i.e., works only when we are specifying the src attribute in<img src='http://programmingashram.in/images/javascript-defer.png' class='img-thumbnail'>",
-       "blogid":"blog5"
-   },
-   blogList6:{
-       "image":"http://programmingashram.in/images/javascript-defer.png",
-       "Title":"jQuery Version 3.6.0 Released!",
-       "Descriptionshort":"jQuery 3.6.0 has been released. jQuery is a lightweight, write less",
-       "Descriptionfull":", do more,JavaScript library.jQuery takes a lot of common tasks that require many lines of JavaScript code to accomplish,and wraps them into methods that you can call with a single line of code.",
-       "blogid":"blog6"
-   },
+    blogList1: {
+         "Title":"JavaScript latest Version in 2021.",
+         "image":"https://venturebeat.com/wp-content/uploads/2018/01/javascript.jpg?fit=1800%2C1116&strip=all",
+         "Descriptionshort":"ECMAScript is the official name of the language. ECMAScript",
+         "Descriptionfull":" versions have been abbreviated to ES1, ES2, ES3, ES5, and ES6. Since 2016 new versions are named by year (ECMAScript 2016 / 2017 / 2018).",
+         "blogid":"blog1",
+         "lang": "js"
+      },
+      blogList2:{
+         "Title":"Most asking question is what is current Bootsrap version ?",
+         "image":"https://labs.tadigital.com/wp-content/uploads/2020/03/MicrosoftTeams-image.png",
+         "Descriptionshort":"Bootstrap 5 stable was released on May 5, 2021.",
+         "Descriptionfull":"  Bootstrap  5 beta 3 was released on March 22, 2021. Bootstrap 5 beta 2 was released on February 10, 2021. Bootstrap 5 beta 1 was released on December 7, 2020.",
+         "blogid":"blog2",
+         "lang": "bootstrap"
+      },
+      blogList3: {
+         "Title":"Most asking question in interview which is current PHP version ?",
+         "image":"https://sdtimes.com/wp-content/uploads/2015/07/0715.sdt-php7.jpg",
+         "Descriptionshort":"Currently, the latest stable version is PHP 7.4. ",
+         "Descriptionfull":"Supported PHP versions and lifecycle.",
+         "blogid":"blog3",
+         "lang": "php"
+      },
+      blogList4:{
+         "Title":"What is Sass ?",
+         "image":"https://images.iambacon.co.uk/blog/sass.png",
+         "Descriptionshort":"Sass is a preprocessor scripting language that is interpreted ",
+         "Descriptionfull":"or compiled into Cascading Style Sheets. SassScript is the scripting language itself. Sass consists of two syntaxes. The original syntax, called 'the indented syntax,' uses a syntax similar to Haml.",
+         "blogid":"blog4",
+         "lang": "sass"
+      },
+      blogList5:{
+        "image":"http://programmingashram.in/images/javascript-defer.png",
+        "Title":"defer in javascript.",
+        "Descriptionshort":"The defer is a Boolean value, used to indicate that script is executed after",
+        "Descriptionfull":"the document has been parsed. It works only with external scripts (i.e., works only when we are specifying the src attribute in<img src='http://programmingashram.in/images/javascript-defer.png' class='img-thumbnail'>",
+        "blogid":"blog5",
+        "lang": "js"
+    },
    
-};
+    
+ };
 let blogs = document.getElementById("blog-details");
+let appBlog = document.getElementById('blogArea');
+let feature = document.getElementById('blogFeature');
+
+for(let p in blog){
+    feature.innerHTML +=`
+    <div class="blog rounded">
+        <a class="nav-link  blog-card p-0 shadow-sm" data-bs-toggle="collapse" href="#${blog[p].blogid}" role="button" aria-expanded="false" aria-controls="collapseExample">
+            <div class="d-flex justify-content-start align-items-start px-2">
+                <div class="news-icon">
+                    <i class="fa-brands h1 mb-0 fa-${blog[p].lang} text-${blog[p].lang}"></i>
+                </div>
+                <div class="head-details  p-3">
+                    <div class="title-header">
+                        <h1 class="h6 mb-1 text-dark">${blog[p].Title}</h1>
+                    </div>
+                    <p class="text-secondary">
+                        ${blog[p].Descriptionshort}
+                    </p>
+                </div>
+            </div>
+        </a>
+        <div class="collapse" id=""${blog[p].blogid}">
+            <div class="card p-3 card-body">
+                ${blog[p].Descriptionfull}
+            </div>
+        </div>
+    </div>
+    `;
+}
+
+for(let o in blog){
+    appBlog.innerHTML +=`
+    <div class="blog rounded">
+        <a class="nav-link  blog-card p-0 shadow-sm" data-bs-toggle="collapse" href="${blog[o].blogid}" role="button" aria-expanded="false" aria-controls="collapseExample">
+            <div class="d-flex justify-content-start align-items-start px-2">
+                <div class="news-icon">
+                    <i class="fa-brands h1 mb-0 fa-${blog[o].lang} text-${blog[o].lang}"></i>
+                </div>
+                <div class="head-details  p-3">
+                    <div class="title-header">
+                        <h1 class="h6 mb-1 text-dark">${blog[o].Title}</h1>
+                    </div>
+                    <p class="text-secondary">
+                        ${blog[o].Descriptionshort}
+                    </p>
+                </div>
+            </div>
+        </a>
+        <div class="collapse" id=""${blog[o].blogid}">
+            <div class="card p-3 card-body">
+                ${blog[o].Descriptionfull}
+            </div>
+        </div>
+    </div>
+ `;
+}
 
 for(let i in blog){
  blogs.innerHTML += `
@@ -61,8 +116,9 @@ for(let i in blog){
             </div>
         </div>
     </a>
-</li>
- `;
+</li>`;
+
+
 }
 
 let ashramLanguage = {
